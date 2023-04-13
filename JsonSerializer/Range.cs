f470eaf3c1e442f669ec1c20025a2fa2acd4ac09
YarikVor <yarikhelov@gmail.com>
2023-04-13 20:48:44 +0300

    Add Range

---
+++ JsonSerializer/Range.cs
@@ -0,0 +1,15 @@
+namespace JsonSerializer;
+
+public struct Range
+{
+    public int start;
+    public int end;
+
+    public Range(int start, int end)
+    {
+        if (start < end) throw new ArgumentException();
+
+        this.start = start;
+        this.end = end;
+    }
+}
