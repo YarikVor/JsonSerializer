6d28285b5051a551cfd4387a630a6c615ebefc48
YarikVor <yarikhelov@gmail.com>
2023-04-13 20:38:09 +0300

    Change tests

--- JsonSerializer.Tests/JsonSerializerTests.cs
+++ JsonSerializer.Tests/JsonSerializerTests.cs
@@ -2,10 +2,10 @@ namespace JsonSerializer.Tests;
 
 public class JsonSerializerTests
 {
-
     public static IEnumerable<object[]> DataForJsonSerializer_Valid = new[]
     {
-        new object[2]{new int[]{1, 2, 3, 4, 5}, "[1,2,3,4,5]"}
+        new object[2] { new[] { 1, 2, 3, 4, 5 }, "[1,2,3,4,5]" },
+        new object[2] { new { value = 23 }, "{'value':23}" }
     };
 
     [Theory]
@@ -14,8 +14,8 @@ public class JsonSerializerTests
     {
         var serializer = new JsonSerializer();
 
-        string actual = serializer.Serialize(obj);
-        
+        var actual = serializer.Serialize(obj);
+
         Assert.Equal(expected, actual);
     }
 }
