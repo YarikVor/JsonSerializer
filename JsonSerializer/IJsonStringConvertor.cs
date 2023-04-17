559b480fbecd0399794b6a2754fea1cd1ec115ec
YarikVor <yarikhelov@gmail.com>
2023-04-17 11:09:26 +0300

    Add ReplaceString.cs

--- JsonSerializer/IJsonStringConvertor.cs
+++
@@ -1,7 +0,0 @@
-namespace JsonSerializer;
-
-public interface IJsonStringConvertor<T>
-{
-    string Write(T value);
-    T Read(string str);
-}
--- JsonSerializer/ReplaceString.cs
+++ JsonSerializer/ReplaceString.cs
@@ -4,19 +4,24 @@ namespace JsonSerializer;
 
 public static class ReplaceString
 {
-    static readonly Dictionary<char, string> ReplaceDictionary = new()
+    private static readonly Dictionary<char, string> EscapeCharDictionary = new()
     {
         { '\0', @"\0" },
         { '\r', @"\r" },
         { '\n', @"\n" },
         { '\\', @"\\" },
-        { '\t', @"\t" }
+        { '\t', @"\t" },
+        { '\b', @"\b" },
+        { '\f', @"\f" },
+        { '\v', @"\v" },
+        { '\a', @"\a" },
+        { '\'', @"\'" }
     };
 
     public static string Replace(string? str)
     {
-        return str == null 
-            ? string.Empty 
+        return str == null
+            ? string.Empty
             : ReplaceAsStringBuilder(str).ToString();
     }
 
@@ -25,11 +30,23 @@ public static class ReplaceString
         var sb = new StringBuilder(str.Length * 2);
 
         foreach (var c in str)
-            if (ReplaceDictionary.TryGetValue(c, out var replaceStr))
+            if (EscapeCharDictionary.TryGetValue(c, out var replaceStr))
                 sb.Append(replaceStr);
             else
                 sb.Append(c);
 
         return sb;
+    }
+
+    public static string ReplaceChar(char c)
+    {
+        return EscapeCharDictionary.TryGetValue(c, out var replaceStr)
+            ? replaceStr
+            : c.ToString();
+    }
+
+    public static bool IsEscapeChar(char c)
+    {
+        return EscapeCharDictionary.ContainsKey(c);
     }
 }
